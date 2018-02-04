0ad63222ebca91ab0830a00bf398a1bd25b61e21
vitalij <laowai121@gmail.com>
2018-02-04 23:57:20 +0000

    added local img and script

---
+++ server/src/main/resources/static/js/create_or_join_game.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#battleship-app',
+    data: {
+
+    },
+    methods: {
+        createNewGameClicked: function () {
+
+        },
+        joinExistingGameClicked: function () {
+
+        }
+    }
+});
