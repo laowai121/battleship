33b9117789a9f42575261106f87425ae2eda021e
vitalij <laowai121@gmail.com>
2018-02-05 23:32:35 +0000

    added font awesome; added create new game form; added back to main menu button

--- server/src/main/resources/static/js/create_or_join_game.js
+++ server/src/main/resources/static/js/create_or_join_game.js
@@ -1,16 +1,23 @@
+var FORM = {
+    MAIN_MENU: 'mainMenu',
+    CREATE_NEW_GAME: 'createNewGame',
+    JOIN_EXISTING_GAME: 'joinExistingGame'
+};
+
 new Vue({
     el: '#battleship-app',
     data: {
-        createOrJoinGameFormShown: true,
-        createNewGameFormShown: false
+        formDisplayed: FORM.MAIN_MENU
     },
     methods: {
         createNewGameClicked: function () {
-            this.createOrJoinGameFormShown = false;
-            this.createNewGameFormShown = true;
+            this.formDisplayed = FORM.CREATE_NEW_GAME;
+        },
+        backToMenuClicked: function () {
+            this.formDisplayed = FORM.MAIN_MENU;
         },
         joinExistingGameClicked: function () {
-
+            this.formDisplayed = FORM.JOIN_EXISTING_GAME;
         }
     }
 });
