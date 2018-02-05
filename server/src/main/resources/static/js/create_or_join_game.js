2485022cd3e0e444cb61d4091e3b2f9c42234085
vitalij <laowai121@gmail.com>
2018-02-05 20:08:21 +0000

    create new game button

--- server/src/main/resources/static/js/create_or_join_game.js
+++ server/src/main/resources/static/js/create_or_join_game.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#battleship-app',
     data: {
-
+        createOrJoinGameFormShown: true,
+        createNewGameFormShown: false
     },
     methods: {
         createNewGameClicked: function () {
-
+            this.createOrJoinGameFormShown = false;
+            this.createNewGameFormShown = true;
         },
         joinExistingGameClicked: function () {
 
