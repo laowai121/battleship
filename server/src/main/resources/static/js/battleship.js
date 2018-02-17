eacb85dd72721cac21f4c68663732db93ec6d1af
vitalij <laowai121@gmail.com>
2018-02-17 10:48:45 +0000

    moved game menu into separate component

--- server/src/main/resources/static/js/battleship.js
+++ server/src/main/resources/static/js/battleship.js
@@ -3,14 +3,6 @@ var BATTLESHIP_PAGE = {
     BATTLESHIP_GAME: 'battleshipGame'
 };
 
-var MENU_FORM = {
-    MAIN: 'main',
-    CREATE_GAME: 'createGame',
-    JOIN_GAME: 'joinGame'
-};
-
-var MAX_SPECTATORS_UNLIMITED = -1;
-
 // from https://github.com/AStaroverov/vue-toast
 var VueToast = window.vueToasts ? window.vueToasts.default || window.vueToasts : window.vueToasts;
 
@@ -18,130 +10,15 @@ var battleshipApp = new Vue({
     el: '#battleship-app',
     data: {
         pageDisplayed: BATTLESHIP_PAGE.MAIN_MENU,
-
-        mainMenu: {
-            loading: false,
-            formDisplayed: MENU_FORM.MAIN,
-            createGameForm: {
-                playerName: {
-                    value: '',
-                    invalid: false,
-                    validator: Validators.playerNameValidator
-                },
-                maxSpectators: {
-                    value: MAX_SPECTATORS_UNLIMITED
-                },
-                validationError: ''
-            },
-            joinGameForm: {
-                playerName: {
-                    value: '',
-                    invalid: false,
-                    validator: Validators.playerNameValidator
-                },
-                gameKey: {
-                    value: '',
-                    invalid: false,
-                    validator: Validators.gameKeyValidator
-                },
-                validationError: ''
-            }
-        },
-
         playerToken: '',
         gameKey: ''
     },
     components: {
-        VueToast: VueToast
+        'vue-toast': VueToast,
+        'main-menu': MainMenu
     },
     created: function () {
         var that = this;
-
-        this.mainMenuResetAllForms = function () {
-            that.mainMenu.createGameForm.playerName.value = '';
-            that.mainMenu.createGameForm.playerName.invalid = false;
-            that.mainMenu.createGameForm.maxSpectators.value = MAX_SPECTATORS_UNLIMITED;
-            that.mainMenu.joinGameForm.playerName.value = '';
-            that.mainMenu.joinGameForm.playerName.invalid = false;
-            that.mainMenu.joinGameForm.gameKey.value = '';
-            that.mainMenu.joinGameForm.gameKey.invalid = false;
-            that.mainMenu.createGameForm.validationError = '';
-            that.mainMenu.joinGameForm.validationError = '';
-        };
-
-        this.validateForm = function (form) {
-            var validationError = null;
-
-            Object.getOwnPropertyNames(form).forEach(function (fieldName) {
-                var validation;
-
-                var field = form[fieldName];
-
-                if (field.validator != null) {
-                    validation = field.validator.validate(field.value);
-                    field.value = validation.normalized;
-
-                    if (validationError != null) {
-                        field.invalid = false;
-                    } else {
-                        if (validation.valid) {
-                            field.invalid = false;
-                        } else {
-                            field.invalid = true;
-                            validationError = validation.error;
-                        }
-                    }
-                }
-            });
-
-            form.validationError = validationError == null ? '' : validationError;
-
-            return validationError == null;
-        };
-
-        this.createGame = function (asSpectator) {
-            var formValid = that.validateForm(that.mainMenu.createGameForm);
-
-            if (formValid) {
-                that.mainMenu.loading = true;
-
-                battleshipApi.createGame(
-                    that.mainMenu.createGameForm.playerName.value,
-                    that.mainMenu.createGameForm.maxSpectators.value,
-                    asSpectator,
-                    function (playerToken, gameKey) {
-                        that.mainMenu.loading = false;
-                        that.openGame(playerToken, gameKey);
-                    },
-                    function (errorMessage) {
-                        that.mainMenu.loading = false;
-                        that.mainMenu.createGameForm.validationError = errorMessage;
-                    }
-                );
-            }
-        };
-
-        this.joinGame = function (asSpectator) {
-            var formValid = that.validateForm(that.mainMenu.joinGameForm);
-
-            if (formValid) {
-                that.mainMenu.loading = true;
-
-                battleshipApi.joinGame(
-                    that.mainMenu.joinGameForm.playerName.value,
-                    that.mainMenu.joinGameForm.gameKey.value,
-                    asSpectator,
-                    function (playerToken, gameKey) {
-                        that.mainMenu.loading = false;
-                        that.openGame(playerToken, gameKey);
-                    },
-                    function (errorMessage) {
-                        that.mainMenu.loading = false;
-                        that.mainMenu.joinGameForm.validationError = errorMessage;
-                    }
-                );
-            }
-        };
 
         this.openGame = function (playerToken, gameKey) {
             that.playerToken = playerToken;
@@ -150,8 +27,8 @@ var battleshipApp = new Vue({
         };
 
         this.showToast = function (message) {
-            this.$refs.toast.showToast(message, {
-                theme: 'success',
+            that.$refs.toast.showToast(message, {
+                theme: 'error',
                 timeLife: 5000,
                 closeBtn: false
             });
@@ -160,42 +37,10 @@ var battleshipApp = new Vue({
     mounted: function () {
         this.$refs.toast.setOptions({
             maxToasts: 6,
-            position: 'bottom right'
+            position: 'top right'
         });
     },
     methods: {
-        openCreateGameMenu: function () {
-            this.mainMenu.formDisplayed = MENU_FORM.CREATE_GAME;
-        },
-        openJoinGameMenu: function () {
-            this.mainMenu.formDisplayed = MENU_FORM.JOIN_GAME;
-        },
-        backToMainMenu: function () {
-            this.mainMenu.formDisplayed = MENU_FORM.MAIN;
-            this.mainMenuResetAllForms();
-        },
-        createGameButtonClicked: function () {
-            this.createGame(false);
-        },
-        createGameAsSpectatorButtonClicked: function () {
-            this.createGame(true);
-        },
-        joinGameButtonClicked: function () {
-            this.joinGame(false);
-        },
-        joinGameAsSpectatorButtonClicked: function () {
-            this.joinGame(true);
-        },
-        createGameTextInputKeyDown: function (e) {
-            if (e.keyCode === 13) {
-                this.createGameButtonClicked();
-            }
-        },
-        joinGameTextInputKeyDown: function (e) {
-            if (e.keyCode === 13) {
-                this.joinGameButtonClicked();
-            }
-        },
         copyGameKey: function () {
             if (copyTextToClipboard(this.gameKey)) {
                 this.showToast('Game Key has been copied to clipboard');
---
+++ server/src/main/resources/static/js/main-menu.js
@@ -0,0 +1,296 @@
+var MENU_FORM = {
+    MAIN: 'main',
+    CREATE_GAME: 'createGame',
+    JOIN_GAME: 'joinGame'
+};
+
+var MAX_SPECTATORS_UNLIMITED = -1;
+
+var MainMenu = {
+    template: '<div class="battleship-container">' +
+              '    <div class="spinner-wrapper" v-show="loading">' +
+              '        <i class="spinner fas fa-spinner fa-spin"></i>' +
+              '    </div>' +
+              '    <div class="main-menu-container" v-show="formDisplayed == MENU_FORM.MAIN">' +
+              '        <h2>Battleship Game</h2>' +
+              '        <div class="row" style="margin-top: 21px;">' +
+              '            <div class="col-md-12">' +
+              '                <button v-on:click="openCreateGameMenu()" v-bind:disabled="loading"' +
+              '                        type="button" class="btn btn-primary btn-block">Create New Game</button>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="row" style="margin-top: 5px;">' +
+              '            <div class="col-md-12">' +
+              '                <button v-on:click="openJoinGameMenu" v-bind:disabled="loading"' +
+              '                        type="button" class="btn btn-warning btn-block">Join Existing Game</button>' +
+              '            </div>' +
+              '        </div>' +
+              '    </div>' +
+              '    <div class="create-game-menu-container" v-show="formDisplayed == MENU_FORM.CREATE_GAME">' +
+              '        <div class="row" style="margin-bottom: 16px;">' +
+              '            <div class="form-group">' +
+              '                <label class="col-md-3" for="create-game-player-name">Player</label>' +
+              '                <div class="col-md-9">' +
+              '                    <input v-model="createGameForm.playerName.value"' +
+              '                           v-bind:disabled="loading"' +
+              '                           v-bind:class="{ \'validation-error\': createGameForm.playerName.invalid }"' +
+              '                           v-on:keydown="createGameTextInputKeyDown"' +
+              '                           type="text" class="form-control" id="create-game-player-name"' +
+              '                           placeholder="Enter Player Name" />' +
+              '                </div>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="row" style="margin-bottom: 16px;">' +
+              '            <div class="form-group">' +
+              '                <label class="col-md-3" for="max-spectators-select">Max Spectators</label>' +
+              '                <div class="col-md-9">' +
+              '                    <select class="form-control" id="max-spectators-select"' +
+              '                            v-model="createGameForm.maxSpectators.value"' +
+              '                            v-bind:disabled="loading">' +
+              '                        <option value="-1">Unlimited</option>' +
+              '                        <option v-for="n in 17" v-bind:value="n - 1">{{ n - 1 }}</option>' +
+              '                    </select>' +
+              '                </div>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="validation-error-container">' +
+              '            {{ createGameForm.validationError }}' +
+              '        </div>' +
+              '        <div class="row">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="createGameButtonClicked"' +
+              '                        type="button" class="btn btn-primary btn-block">Create Game</button>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="row" style="margin-top: 4px;">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="createGameAsSpectatorButtonClicked"' +
+              '                        type="button" class="btn btn-warning btn-block">Create Game as Spectator</button>' +
+              '            </div>' +
+              '        </div>' +
+              '        <hr class="menu-divider" />' +
+              '        <div class="row">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="backToMainMenu"' +
+              '                        type="button" class="btn btn-danger btn-block">' +
+              '                    <i class="fas fa-arrow-left"></i> Back to Main Menu' +
+              '                </button>' +
+              '            </div>' +
+              '        </div>' +
+              '    </div>' +
+              '    <div class="join-game-menu-container" v-show="formDisplayed == MENU_FORM.JOIN_GAME">' +
+              '        <div class="row" style="margin-bottom: 4px;">' +
+              '            <div class="form-group">' +
+              '                <label class="col-md-2" for="join-game-player-name">Player</label>' +
+              '                <div class="col-md-10">' +
+              '                    <input v-model="joinGameForm.playerName.value"' +
+              '                           v-bind:disabled="loading"' +
+              '                           v-bind:class="{ \'validation-error\': joinGameForm.playerName.invalid }"' +
+              '                           v-on:keydown="joinGameTextInputKeyDown"' +
+              '                           type="text" class="form-control" id="join-game-player-name"' +
+              '                           placeholder="Enter Player Name" />' +
+              '                </div>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="row" style="margin-bottom: 16px;">' +
+              '            <div class="form-group">' +
+              '                <label class="col-md-2" for="join-game-game-key">Key</label>' +
+              '                <div class="col-md-10">' +
+              '                    <input v-model="joinGameForm.gameKey.value"' +
+              '                           v-bind:disabled="loading"' +
+              '                           v-bind:class="{ \'validation-error\': joinGameForm.gameKey.invalid }"' +
+              '                           v-on:keydown="joinGameTextInputKeyDown"' +
+              '                           type="text" class="form-control" id="join-game-game-key"' +
+              '                           placeholder="Enter Game Key" />' +
+              '                </div>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="validation-error-container">' +
+              '            {{ joinGameForm.validationError }}' +
+              '        </div>' +
+              '        <div class="row">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="joinGameButtonClicked"' +
+              '                        type="button" class="btn btn-primary btn-block">Join Game</button>' +
+              '            </div>' +
+              '        </div>' +
+              '        <div class="row" style="margin-top: 4px;">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="joinGameAsSpectatorButtonClicked"' +
+              '                        type="button" class="btn btn-warning btn-block">Join Game as Spectator</button>' +
+              '            </div>' +
+              '        </div>' +
+              '        <hr class="menu-divider" />' +
+              '        <div class="row">' +
+              '            <div class="col-md-12">' +
+              '                <button v-bind:disabled="loading"' +
+              '                        v-on:click="backToMainMenu"' +
+              '                        type="button" class="btn btn-danger btn-block">' +
+              '                    <i class="fas fa-arrow-left"></i> Back to Main Menu' +
+              '                </button>' +
+              '            </div>' +
+              '        </div>' +
+              '    </div>' +
+              '</div>',
+
+    data: function () {
+        return {
+            loading: false,
+            formDisplayed: MENU_FORM.MAIN,
+            createGameForm: {
+                playerName: {
+                    value: '',
+                    invalid: false,
+                    validator: Validators.playerNameValidator
+                },
+                maxSpectators: {
+                    value: MAX_SPECTATORS_UNLIMITED
+                },
+                validationError: ''
+            },
+            joinGameForm: {
+                playerName: {
+                    value: '',
+                    invalid: false,
+                    validator: Validators.playerNameValidator
+                },
+                gameKey: {
+                    value: '',
+                    invalid: false,
+                    validator: Validators.gameKeyValidator
+                },
+                validationError: ''
+            }
+        }
+    },
+
+    created: function () {
+        var that = this;
+
+        this.resetAllForms = function () {
+            that.createGameForm.playerName.value = '';
+            that.createGameForm.playerName.invalid = false;
+            that.createGameForm.maxSpectators.value = MAX_SPECTATORS_UNLIMITED;
+            that.joinGameForm.playerName.value = '';
+            that.joinGameForm.playerName.invalid = false;
+            that.joinGameForm.gameKey.value = '';
+            that.joinGameForm.gameKey.invalid = false;
+            that.createGameForm.validationError = '';
+            that.joinGameForm.validationError = '';
+        };
+
+        this.validateForm = function (form) {
+            var validationError = null;
+
+            Object.getOwnPropertyNames(form).forEach(function (fieldName) {
+                var validation;
+
+                var field = form[fieldName];
+
+                if (field.validator != null) {
+                    validation = field.validator.validate(field.value);
+                    field.value = validation.normalized;
+
+                    if (validationError != null) {
+                        field.invalid = false;
+                    } else {
+                        if (validation.valid) {
+                            field.invalid = false;
+                        } else {
+                            field.invalid = true;
+                            validationError = validation.error;
+                        }
+                    }
+                }
+            });
+
+            form.validationError = validationError == null ? '' : validationError;
+
+            return validationError == null;
+        };
+
+        this.createGame = function (asSpectator) {
+            var formValid = that.validateForm(that.createGameForm);
+
+            if (formValid) {
+                that.loading = true;
+
+                battleshipApi.createGame(
+                    that.createGameForm.playerName.value,
+                    that.createGameForm.maxSpectators.value,
+                    asSpectator,
+                    function (playerToken, gameKey) {
+                        that.loading = false;
+                        that.$root.openGame(playerToken, gameKey);
+                    },
+                    function (errorMessage) {
+                        that.loading = false;
+                        that.createGameForm.validationError = errorMessage;
+                    }
+                );
+            }
+        };
+
+        this.joinGame = function (asSpectator) {
+            var formValid = that.validateForm(that.joinGameForm);
+
+            if (formValid) {
+                that.loading = true;
+
+                battleshipApi.joinGame(
+                    that.joinGameForm.playerName.value,
+                    that.joinGameForm.gameKey.value,
+                    asSpectator,
+                    function (playerToken, gameKey) {
+                        that.loading = false;
+                        that.$root.openGame(playerToken, gameKey);
+                    },
+                    function (errorMessage) {
+                        that.loading = false;
+                        that.joinGameForm.validationError = errorMessage;
+                    }
+                );
+            }
+        }
+    },
+
+    methods: {
+        openCreateGameMenu: function () {
+            this.formDisplayed = MENU_FORM.CREATE_GAME;
+        },
+        openJoinGameMenu: function () {
+            this.formDisplayed = MENU_FORM.JOIN_GAME;
+        },
+        backToMainMenu: function () {
+            this.formDisplayed = MENU_FORM.MAIN;
+            this.resetAllForms();
+        },
+        createGameButtonClicked: function () {
+            this.createGame(false);
+        },
+        createGameAsSpectatorButtonClicked: function () {
+            this.createGame(true);
+        },
+        joinGameButtonClicked: function () {
+            this.joinGame(false);
+        },
+        joinGameAsSpectatorButtonClicked: function () {
+            this.joinGame(true);
+        },
+        createGameTextInputKeyDown: function (e) {
+            if (e.keyCode === 13) {
+                this.createGameButtonClicked();
+            }
+        },
+        joinGameTextInputKeyDown: function (e) {
+            if (e.keyCode === 13) {
+                this.joinGameButtonClicked();
+            }
+        }
+    }
+};
